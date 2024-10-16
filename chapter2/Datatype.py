912473f3b70799885105777563e4ef939929ac22
Parth Champaneri <parth.c@buckbox.tech>
2024-10-16 18:02:26 +0530

    add all files

--- chapter2/Datatype.py
+++ chapter2/Datatype.py
@@ -21,4 +21,4 @@ print("True and True is ", True and True)
 print("False and True is ", False and True)
 print("False and False is ", False and False)
 
-print(not(True))
+print(not(False))
---
+++ chapter4/chapter4 PS/problem3.py
@@ -0,0 +1,4 @@
+# 3. Check that a tuple type cannot be changed in python.
+
+a = (15,25,"parth")
+a[2]= "blabla"
